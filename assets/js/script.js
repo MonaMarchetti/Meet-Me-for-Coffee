7a23e4a64c8a548b35d278b3cb390f5ca9d0542f
MonaMarchetti <marchetti.mona@gmail.com>
2019-02-20 15:55:56 +0100

    structure et look globaux

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,45 @@
 (function() {
 
-  userName = prompt("What's your name ?");
+// récupération du prénom du visiteur pour un accueil interractif
 
-  alert("Hello " + userName + " !");
+  //userName = prompt("What's your name ?");
 
-  // récupération du prénom du visiteur pour un accueil interractif
+  //alert("Hello " + userName + " !");
+
+//affichage avec un 0 devant
+      function pad2(number) {
+      return (number < 10 ? '0' : '') + number;
+      }
+// ouverture/fermeture en fonction de l'heure
+
+      var d = new Date();
+
+      var semaine = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"];
+
+      var jour = semaine[d.getDay()-1];
+
+      var datec = d.getDate();
+
+      var an = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
+
+      var mois = an[d.getMonth()];
+
+      var annee = d.getFullYear();
+
+      var heure = d.getHours();
+
+      var minute = d.getMinutes();
+
+      document.getElementById("welcome").innerHTML = (jour + " " + datec + " " + mois + " " + annee + ", " + heure + "h" + pad2(minute));
+
+      if (heure < 18 && minute < 30) {
+        document.getElementById("target").innerHTML = "ouvert <i class='fas fa-heart'></i> ";
+        }
+      else if (heure < 17) {
+        document.getElementById("target").innerHTML = "ouvert <i class='fas fa-heart'></i>";
+      }
+      else {
+        document.getElementById("target").innerHTML = "fermé <i class='fas fa-heart-broken'></i>";
+      }
 
 })();
